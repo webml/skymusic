31c3650fb177d7be44f94e9d10b73081b6312875
Misha Lubarets <lumek@icloud.com>
2025-08-08 18:18:56 +0300

    fix paths

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import viteTSConfig from "vite-tsconfig-paths";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint"],
   css: ["~/assets/styles/main.css", "~/assets/styles/reset.css"],
+  vite: {
+    plugins: [viteTSConfig()],
+  },
 });
