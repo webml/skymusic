ea2c139947f6ed8af8ad17a910b16a34550f1629
Misha Lubarets <lumek@icloud.com>
2025-09-02 19:05:21 +0300

    add 404 and tracks loading

---
+++ components/NotFound.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <h1 class="title">404</h1>
+    <p class="subtitle">Страница не найдена 😞</p>
+    <p class="description">
+      Возможно, она была удалена или перенесена на другой адрес
+    </p>
+    <button class="btn" @click="$router.push('/')">Вернуться на главную</button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: transparent;
+  color: white;
+  font-family: Arial, sans-serif;
+  text-align: center;
+}
+
+.title {
+  font-size: 100px;
+  margin: 0;
+}
+
+.subtitle {
+  font-size: 24px;
+  margin-top: 20px;
+}
+
+.description {
+  font-size: 18px;
+  margin-top: 10px;
+}
+
+.btn {
+  padding: 15px 30px;
+  border: none;
+  border-radius: 5px;
+  background-color: purple;
+  color: white;
+  font-size: 18px;
+  cursor: pointer;
+  margin-top: 30px;
+}
+</style>
---
+++ components/PlaylistTrackLoading.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="playlist__item">
+    <div class="playlist__track track">
+      <div class="track__title" @click="setTrack">
+        <div class="track__title-image"></div>
+        <div class="track__title-text"></div>
+      </div>
+      <div class="track__author">
+        <div class="track__title-text"></div>
+      </div>
+      <div class="track__album">
+        <div class="track__title-text"></div>
+      </div>
+      <div class="track__time">
+        <span class="track__time-text">
+          <div class="track__title-text"></div
+        ></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.playlist__item {
+  width: 100%;
+  display: block;
+  margin-bottom: 12px;
+}
+
+.playlist__track {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  -webkit-box-pack: justify;
+  -ms-flex-pack: justify;
+  justify-content: space-between;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+}
+
+.track__title {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  width: 447px;
+}
+
+.track__title-image {
+  width: 51px;
+  height: 51px;
+  padding: 16px;
+  background: #313131;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  margin-right: 17px;
+}
+
+.track__title-text {
+  height: 19px;
+  width: 100%;
+  background: #313131;
+}
+
+.track__title-svg {
+  width: 18px;
+  height: 17px;
+  fill: transparent;
+  stroke: #4e4e4e;
+}
+
+.track__title-link {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: #ffffff;
+}
+
+.track__title-span {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: #4e4e4e;
+}
+
+.track__author {
+  width: 321px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-pack: start;
+  -ms-flex-pack: start;
+  justify-content: flex-start;
+}
+
+.track__author-link {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: #ffffff;
+  text-align: left;
+}
+
+.track__album {
+  width: 245px;
+}
+
+.track__album-link {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: #696969;
+}
+
+.track__time {
+  width: 65px;
+  background: #313131;
+}
+
+.track__time-svg {
+  width: 14px;
+  height: 12px;
+  margin-right: 17px;
+  fill: transparent;
+  stroke: #696969;
+}
+
+.track__time-text {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  text-align: right;
+  color: #696969;
+}
+</style>
